f2251df29bf4795ffe3152538d8471e3ce1d2687
Krzysztof Murczyński <soulinek@gmail.com>
2017-01-22 05:32:24 +0100

    as

--- Assets/Room.cs
+++ Assets/Room.cs
@@ -8,6 +8,8 @@ public class Room : MonoBehaviour {
     bool StartExit;
     float dampTime;
     float dampProgress;
+    public Transform[] masks;
+    bool canQuit;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,7 @@ public class Room : MonoBehaviour {
         dampTime = 0;
         dampProgress = 0;
         StartExit = true;
+        canQuit = true;
 	}
 	
 	// Update is called once per frame
@@ -35,18 +38,27 @@ public class Room : MonoBehaviour {
         {
             Debug.Log("bla");
             if (StartExit == true)
+            {
+                canQuit = false;
                 MissionStart();
+            }
             else
+            if (canQuit == true)
+            {
                 Application.Quit();
+            }
         }
 	}
 
     void MissionStart()
     {
+        GameObject.Find("Player").GetComponent<PlayerCollisions>().TurnOnMask();
         GameObject.Find("Main Camera").GetComponent<CameraFollow>().shouldFollow = true;
         // Invoke("CameraSizeDamp", 0.05f);
         StartCoroutine(CameraSizeDamp());
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+
+        Destroy(gameObject, 5f);
     }
 
     IEnumerator CameraSizeDamp()
@@ -55,6 +67,11 @@ public class Room : MonoBehaviour {
         {
             dampProgress += 0.05f;
             GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize = (9.2f - (5f * dampProgress));
+            masks[0].localScale = new Vector3((3.3f - (.8f * dampProgress)), 1f, (1.85f - (0.45f * dampProgress)));
+            masks[1].localScale = new Vector3((2f - (.55f * dampProgress)), (1.8f - (.45f * dampProgress)), 1);
+            masks[2].localScale = new Vector3((2f - (.55f * dampProgress)), (1.8f - (.45f * dampProgress)), 1);
+            masks[3].localScale = new Vector3((2f - (.55f * dampProgress)), (1.8f - (.45f * dampProgress)), 1);
+            
             yield return null;
         }
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
